2e46a30bd76f16110322e082da3d3ea5875f24eb
Harish Karthic <harish.karthic@gmail.com>
2020-09-22 11:27:01 +0100

    Introduced Repository pattern

--- Infracture/Data/Migrations/20200922051616_InitialCreate.Designer.cs
+++ Infracture/Data/Migrations/20200922051616_InitialCreate.Designer.cs
@@ -2,14 +2,16 @@
 using Infracture.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infracture.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20200922051616_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Infracture/Data/StoreContext.cs
+++ Infracture/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,5 +12,16 @@ namespace Infracture.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        // Override OnModelCreating method which create migrations and tell it to look of custom configuration inside the project
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // base is the class that we're deriving from; In this case it is DbContext
+            base.OnModelCreating(modelBuilder);
+            // Explicitly telling the model builder to look for configuration inside the project
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
